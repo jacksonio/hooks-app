f14dd7a1b99f2886897a532a876cee4f9acdc945
Jack <56225474+jacksonio@users.noreply.github.com>
2020-04-21 21:23:35 +0300

    Updates

---
+++ precache-manifest.2bdc7aac7388b54af642d73f620f63e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61d027111e91312d3fc31d2daf96b23",
+    "url": "/hooks-app/index.html"
+  },
+  {
+    "revision": "ae76e2430d82fb692f99",
+    "url": "/hooks-app/static/css/main.57a946dc.chunk.css"
+  },
+  {
+    "revision": "42b2156bd85a2a833474",
+    "url": "/hooks-app/static/js/2.19800663.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hooks-app/static/js/2.19800663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae76e2430d82fb692f99",
+    "url": "/hooks-app/static/js/main.b2d69dab.chunk.js"
+  },
+  {
+    "revision": "33e81612a6ca460d9eae",
+    "url": "/hooks-app/static/js/runtime-main.f73d9011.js"
+  }
+]);
